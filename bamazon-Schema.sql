77fd12db07d61c9df8ff269db97f2c8f713f0c1e
Spiderman <santiagosepulveda@me.com>
2018-12-14 22:37:25 -0500

    initial schema

--- bamazon-Schema.sql
+++ bamazon-Schema.sql
@@ -4,10 +4,10 @@ CREATE database bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-  item_id INT NOT NULL,
+  item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NOT NULL,
   department_name VARCHAR(100) NOT NULL,
-  price DECIMAL(10,4) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
   stock_quantity INT NOT NULL,
   PRIMARY KEY (item_id)
 );
